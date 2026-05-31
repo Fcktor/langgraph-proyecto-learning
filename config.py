8b796a36e0a70879e0b8695d77260b4849d9ab06
Fucktor <sk8punkero@gmail.com>
2026-05-31 17:01:56 -0500

    Primer commit del proyecto con langgraph

---
+++ config.py
@@ -0,0 +1,16 @@
+from dotenv import load_dotenv
+from langchain.chat_models import init_chat_model
+from langchain_tavily import TavilySearch 
+from langgraph.checkpoint.memory import MemorySaver
+
+load_dotenv()
+
+memory = MemorySaver()
+tavily_tool = TavilySearch(max_results=2)
+tools = [tavily_tool]
+
+llm = init_chat_model("gpt-4o", temperature=0.9)
+llm_with_tools = llm.bind_tools(tools)
+
+graph_config = {
+    "configurable": {"thread_id": "1"}}
